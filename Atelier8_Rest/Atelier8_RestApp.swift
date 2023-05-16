b3c0eb209fd59e0aef28aeea7272a371e6e9df79
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-05-16 10:28:23 +0100

    Initial Commit

---
+++ Atelier8_Rest/Atelier8_RestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Atelier8_RestApp.swift
+//  Atelier8_Rest
+//
+//  Created by CHEBIHI FAYCAL on 16/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct Atelier8_RestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
